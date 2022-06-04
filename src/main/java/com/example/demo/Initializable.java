852608beb080d8c6b3995437dfe810dfed1d9c66
Karol Zmarz <karol.zmarz2@gmail.com>
2022-06-04 14:59:02 +0200

    Tic-Tac-Toe

---
+++ src/main/java/com/example/demo/Initializable.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public interface Initializable {
+    void initialize(URL url, ResourceBundle resourceBundle);
+}
